5b65eee9a34fc89db6f334d73bc5bc8bc54f7b00
Dave Brophy <dave@brophy.uk>
2026-09-10 14:52:38 +0200

    Upgrade jennifer to v1.7.1
    Also commit the generated import block as gendst writes it, rather than
    the goimports-grouped version that was committed previously, so that
    running the generator leaves a clean tree. That makes it possible to
    check the generated files are up to date with:
    
        go run ./gendst && git diff --exit-code

--- decorator/restorer-generated.go
+++ decorator/restorer-generated.go
@@ -2,10 +2,9 @@ package decorator
 
 import (
 	"fmt"
+	"github.com/dave/dst"
 	"go/ast"
 	"go/token"
-
-	"github.com/dave/dst"
 )
 
 func (r *FileRestorer) restoreNode(n dst.Node, parentName, parentField, parentFieldType string, allowDuplicate bool) ast.Node {
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/dave/dst
 go 1.26.0
 
 require (
-	github.com/dave/jennifer v1.5.0
+	github.com/dave/jennifer v1.7.1
 	github.com/sergi/go-diff v1.2.0
 	golang.org/x/tools v0.50.0
 	gopkg.in/src-d/go-billy.v4 v4.3.2
